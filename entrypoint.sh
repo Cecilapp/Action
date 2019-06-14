afcbe07285993747403769999a4c8b9eb8342d18
Arnaud Ligny <arnaud@ligny.org>
2019-06-15 01:30:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Starting the Cecil Action"
+echo "### Starting the Cecil Action"
 
 curl -SOL https://cecil.app/cecil.phar
 
-sh -c "php cecil.phar build -v $*"
+sh -c "php cecil.phar build $*"
 
-echo "Completed the Cecil Action"
+echo "### Completed the Cecil Action"
