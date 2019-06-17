42f10386ea007853ade0730712b3876c7c70e06c
Arnaud Ligny <arnaud@ligny.org>
2019-06-17 12:14:31 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@ set -e
 
 echo "### Downloading Cecil"
 
-curl -SOL https://cecil.app/cecil.phar
+if [[ -z "$CECIL_VERSION" ]]; then
+	curl -SOL https://cecil.app/cecil.phar
+else
+  curl -SOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+fi
 
 echo "### Building Cecil static site"
 
