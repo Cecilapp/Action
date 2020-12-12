579e70574e2cd74754fbfa43afe239e3075a304f
Arnaud Ligny <arnaud@ligny.org>
2020-12-13 00:56:40 +0100

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM php:7.4-cli-alpine
 
 # i18n
-RUN apk add --no-cache gettext-dev icu-dev \
-&& docker-php-ext-install -j$(nproc) gettext \
-&& docker-php-ext-install -j$(nproc) intl
+#RUN apk add --no-cache gettext-dev icu-dev \
+#&& docker-php-ext-install -j$(nproc) gettext \
+#&& docker-php-ext-install -j$(nproc) intl
 
 ADD entrypoint.sh /
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 set -e
 
+# Install INTL extensions
+if [[ $INPUT_INSTALL_THEMES -eq 'yes' ]]; then
+  apk update
+  apk add --no-cache gettext-dev icu-dev
+  docker-php-ext-install -j$(nproc) gettext
+  docker-php-ext-install -j$(nproc) intl
+fi
+
+# Install theme(s)
+if [[ -f "composer.json" && $INPUT_INSTALL_THEMES = 'yes' ]]; then
+  echo "Installing theme(s)..."
+  #curl -sS https://getcomposer.org/installer | php
+  #php composer.phar install --prefer-dist --no-dev --no-progress
+  composer install --prefer-dist --no-dev --no-progress
+fi
+
+# Download Cecil
 if [[ -z "$CECIL_VERSION" ]]; then
   echo "Downloading Cecil..."
   curl -sSOL https://cecil.app/cecil.phar
@@ -9,4 +26,5 @@ else
   curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
 fi
 
+# Run build
 php cecil.phar build $INPUT_ARGS
