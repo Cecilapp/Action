a01f16eebba3d74291ae5cc72cd583aa3485e072
Arnaud Ligny <arnaud@ligny.org>
2020-12-12 22:14:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,11 @@
 set -e
 
 if [[ -z "$CECIL_VERSION" ]]; then
-  echo "### Downloading Cecil"
+  echo "Downloading Cecil..."
   curl -sSOL https://cecil.app/cecil.phar
 else
-  echo "### Downloading Cecil $CECIL_VERSION"
+  echo "Downloading Cecil $CECIL_VERSION..."
   curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
 fi
 
-echo "### Building Cecil static site"
 php cecil.phar build -v
-echo "### Cecil Action done"
